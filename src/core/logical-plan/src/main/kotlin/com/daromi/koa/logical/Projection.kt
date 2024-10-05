5dbf11f7309f2b1295ebf8e7258085e7fc1a2d18
daromi94 <davromero07@gmail.com>
2024-10-04 21:38:46 -0500

    feat: :sparkles: add projection and selection logical operators

---
+++ src/core/logical-plan/src/main/kotlin/com/daromi/koa/logical/Projection.kt
@@ -0,0 +1,20 @@
+package com.daromi.koa.logical
+
+import com.daromi.koa.datatypes.Schema
+
+data class Projection(
+    val input: LogicalOperator,
+    val expressions: List<LogicalExpression>,
+) : LogicalOperator {
+    override val schema: Schema = deriveSchema()
+
+    override val children: List<LogicalOperator> = listOf(this.input)
+
+    private fun deriveSchema(): Schema {
+        val fields = this.expressions.map { it.toField(this.input) }
+
+        return Schema(fields)
+    }
+
+    override fun toString(): String = "Projection: ${this.expressions}"
+}
