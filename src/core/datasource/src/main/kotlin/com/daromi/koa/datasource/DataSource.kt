079dad05ef9fef83a84982d32533790edab709db
daromi94 <davromero07@gmail.com>
2024-10-09 19:15:25 -0500

    refactor: :recycle: remove datasource name

--- src/core/datasource/src/main/kotlin/com/daromi/koa/datasource/DataSource.kt
+++ src/core/datasource/src/main/kotlin/com/daromi/koa/datasource/DataSource.kt
@@ -4,8 +4,6 @@ import com.daromi.koa.datatypes.RecordBatch
 import com.daromi.koa.datatypes.Schema
 
 interface DataSource {
-    val name: String
-
     val schema: Schema
 
     fun scan(projection: List<String>): Iterator<RecordBatch>
