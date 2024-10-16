62c7caedfd4fbe8229f1a4ad6cbded300d644a55
daromi94 <davromero07@gmail.com>
2024-10-15 20:35:25 -0500

    refactor: :recycle: combine record batch with column vector

--- src/core/datatypes/src/main/kotlin/com/daromi/koa/datatypes/RecordBatch.kt
+++ src/core/datatypes/src/main/kotlin/com/daromi/koa/datatypes/RecordBatch.kt
@@ -1,12 +1,23 @@
 package com.daromi.koa.datatypes
 
+import org.apache.arrow.vector.types.pojo.ArrowType
+
 data class RecordBatch(
     val schema: Schema,
+    val size: Int,
     val columns: List<ColumnVector>,
 ) {
     fun get(index: Int): ColumnVector {
-        assert(index in 0..<this.columns.size)
+        if (index !in 0..<this.columns.size) {
+            throw IndexOutOfBoundsException("column index $index out of bounds")
+        }
 
         return this.columns[index]
     }
+}
+
+interface ColumnVector {
+    val type: ArrowType
+
+    fun get(index: Int): Any
 }
