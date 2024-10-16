891515da4ebc599c169bbefe76e82482d7354e98
daromi94 <davromero07@gmail.com>
2024-10-15 19:18:05 -0500

    feat: :sparkles: add smaller-sized and unsigned integer types

--- src/core/datatypes/src/main/kotlin/com/daromi/koa/datatypes/ArrowTypes.kt
+++ src/core/datatypes/src/main/kotlin/com/daromi/koa/datatypes/ArrowTypes.kt
@@ -5,8 +5,14 @@ import org.apache.arrow.vector.types.pojo.ArrowType
 
 object ArrowTypes {
     val BooleanType = ArrowType.Bool()
+    val Int8Type = ArrowType.Int(8, true)
+    val Int16Type = ArrowType.Int(16, true)
     val Int32Type = ArrowType.Int(32, true)
     val Int64Type = ArrowType.Int(64, true)
+    val UInt8Type = ArrowType.Int(8, false)
+    val UInt16Type = ArrowType.Int(16, false)
+    val UInt32Type = ArrowType.Int(32, false)
+    val UInt64Type = ArrowType.Int(64, false)
     val FloatType = ArrowType.FloatingPoint(FloatingPointPrecision.SINGLE)
     val DoubleType = ArrowType.FloatingPoint(FloatingPointPrecision.DOUBLE)
     val StringType = ArrowType.Utf8()
