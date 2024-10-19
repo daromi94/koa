f8ee143eaeb1e4dc53fee805e6ca5456bbe99e99
daromi94 <davromero07@gmail.com>
2024-10-18 23:06:28 -0500

    style: :art: format logical expressions

--- src/core/logical-plan/src/main/kotlin/com/daromi/koa/logical/LogicalExpressions.kt
+++ src/core/logical-plan/src/main/kotlin/com/daromi/koa/logical/LogicalExpressions.kt
@@ -1,166 +1,180 @@
 package com.daromi.koa.logical
 
-import com.daromi.koa.datatypes.ArrowTypes
 import com.daromi.koa.datatypes.Field
+import com.daromi.koa.datatypes.Types
 
 @JvmInline
-value class Column(
-    val name: String,
-) : LogicalExpression {
-    override fun toField(context: LogicalOperator): Field {
-        val field = context.schema.fields.find { it.name == this.name }
-        if (field == null) {
-            throw NoSuchElementException("unknown column '${this.name}'")
-        }
-        return field
+value class Column(private val name: String) : LogicalExpression {
+
+  override fun toField(context: LogicalOperator): Field {
+    val field = context.schema.field(this.name)
+    if (field == null) {
+      throw NoSuchElementException("unknown column '${this.name}'")
     }
+    return field
+  }
 
-    override fun toString(): String = "#${this.name}"
+  override fun toString(): String = "#${this.name}"
 }
 
-fun col(name: String): Column = Column(name)
+fun col(name: String) = Column(name)
 
 @JvmInline
-value class BooleanLiteral(
-    val value: Boolean,
-) : LogicalExpression {
-    override fun toField(context: LogicalOperator): Field = Field(this.value.toString(), ArrowTypes.BooleanType)
+value class BooleanLiteral(private val value: Boolean) : LogicalExpression {
 
-    override fun toString(): String = this.value.toString()
+  override fun toField(context: LogicalOperator): Field {
+    return Field(this.value.toString(), Types.Boolean)
+  }
+
+  override fun toString(): String = this.value.toString()
 }
 
-fun lit(value: Boolean): BooleanLiteral = BooleanLiteral(value)
+fun lit(value: Boolean) = BooleanLiteral(value)
 
 @JvmInline
-value class Int8Literal(
-    val value: Byte,
-) : LogicalExpression {
-    override fun toField(context: LogicalOperator): Field = Field(this.value.toString(), ArrowTypes.Int8Type)
+value class Int8Literal(private val value: Byte) : LogicalExpression {
 
-    override fun toString(): String = this.value.toString()
+  override fun toField(context: LogicalOperator): Field {
+    return Field(this.value.toString(), Types.Int8)
+  }
+
+  override fun toString(): String = this.value.toString()
 }
 
-fun lit(value: Byte): Int8Literal = Int8Literal(value)
+fun lit(value: Byte) = Int8Literal(value)
 
 @JvmInline
-value class Int16Literal(
-    val value: Short,
-) : LogicalExpression {
-    override fun toField(context: LogicalOperator): Field = Field(this.value.toString(), ArrowTypes.Int16Type)
+value class Int16Literal(private val value: Short) : LogicalExpression {
 
-    override fun toString(): String = this.value.toString()
+  override fun toField(context: LogicalOperator): Field {
+    return Field(this.value.toString(), Types.Int16)
+  }
+
+  override fun toString(): String = this.value.toString()
 }
 
-fun lit(value: Short): Int16Literal = Int16Literal(value)
+fun lit(value: Short) = Int16Literal(value)
 
 @JvmInline
-value class Int32Literal(
-    val value: Int,
-) : LogicalExpression {
-    override fun toField(context: LogicalOperator): Field = Field(this.value.toString(), ArrowTypes.Int32Type)
+value class Int32Literal(private val value: Int) : LogicalExpression {
 
-    override fun toString(): String = this.value.toString()
+  override fun toField(context: LogicalOperator): Field {
+    return Field(this.value.toString(), Types.Int32)
+  }
+
+  override fun toString(): String = this.value.toString()
 }
 
-fun lit(value: Int): Int32Literal = Int32Literal(value)
+fun lit(value: Int) = Int32Literal(value)
 
 @JvmInline
-value class Int64Literal(
-    val value: Long,
-) : LogicalExpression {
-    override fun toField(context: LogicalOperator): Field = Field(this.value.toString(), ArrowTypes.Int64Type)
+value class Int64Literal(private val value: Long) : LogicalExpression {
 
-    override fun toString(): String = this.value.toString()
+  override fun toField(context: LogicalOperator): Field {
+    return Field(this.value.toString(), Types.Int64)
+  }
+
+  override fun toString(): String = this.value.toString()
 }
 
-fun lit(value: Long): Int64Literal = Int64Literal(value)
+fun lit(value: Long) = Int64Literal(value)
 
 @JvmInline
-value class UInt8Literal(
-    val value: UByte,
-) : LogicalExpression {
-    override fun toField(context: LogicalOperator): Field = Field(this.value.toString(), ArrowTypes.UInt8Type)
+value class UInt8Literal(private val value: UByte) : LogicalExpression {
 
-    override fun toString(): String = this.value.toString()
+  override fun toField(context: LogicalOperator): Field {
+    return Field(this.value.toString(), Types.UInt8)
+  }
+
+  override fun toString(): String = this.value.toString()
 }
 
-fun lit(value: UByte): UInt8Literal = UInt8Literal(value)
+fun lit(value: UByte) = UInt8Literal(value)
 
 @JvmInline
-value class UInt16Literal(
-    val value: UShort,
-) : LogicalExpression {
-    override fun toField(context: LogicalOperator): Field = Field(this.value.toString(), ArrowTypes.UInt16Type)
+value class UInt16Literal(private val value: UShort) : LogicalExpression {
 
-    override fun toString(): String = this.value.toString()
+  override fun toField(context: LogicalOperator): Field {
+    return Field(this.value.toString(), Types.UInt16)
+  }
+
+  override fun toString(): String = this.value.toString()
 }
 
-fun lit(value: UShort): UInt16Literal = UInt16Literal(value)
+fun lit(value: UShort) = UInt16Literal(value)
 
 @JvmInline
-value class UInt32Literal(
-    val value: UInt,
-) : LogicalExpression {
-    override fun toField(context: LogicalOperator): Field = Field(this.value.toString(), ArrowTypes.UInt32Type)
+value class UInt32Literal(private val value: UInt) : LogicalExpression {
 
-    override fun toString(): String = this.value.toString()
+  override fun toField(context: LogicalOperator): Field {
+    return Field(this.value.toString(), Types.UInt32)
+  }
+
+  override fun toString(): String = this.value.toString()
 }
 
-fun lit(value: UInt): UInt32Literal = UInt32Literal(value)
+fun lit(value: UInt) = UInt32Literal(value)
 
 @JvmInline
-value class UInt64Literal(
-    val value: ULong,
-) : LogicalExpression {
-    override fun toField(context: LogicalOperator): Field = Field(this.value.toString(), ArrowTypes.UInt64Type)
+value class UInt64Literal(private val value: ULong) : LogicalExpression {
 
-    override fun toString(): String = this.value.toString()
+  override fun toField(context: LogicalOperator): Field {
+    return Field(this.value.toString(), Types.UInt64)
+  }
+
+  override fun toString(): String = this.value.toString()
 }
 
-fun lit(value: ULong): UInt64Literal = UInt64Literal(value)
+fun lit(value: ULong) = UInt64Literal(value)
 
 @JvmInline
-value class FloatLiteral(
-    val value: Float,
-) : LogicalExpression {
-    override fun toField(context: LogicalOperator): Field = Field(this.value.toString(), ArrowTypes.FloatType)
+value class FloatLiteral(private val value: Float) : LogicalExpression {
 
-    override fun toString(): String = this.value.toString()
+  override fun toField(context: LogicalOperator): Field {
+    return Field(this.value.toString(), Types.Float)
+  }
+
+  override fun toString(): String = this.value.toString()
 }
 
-fun lit(value: Float): FloatLiteral = FloatLiteral(value)
+fun lit(value: Float) = FloatLiteral(value)
 
 @JvmInline
-value class DoubleLiteral(
-    val value: Double,
-) : LogicalExpression {
-    override fun toField(context: LogicalOperator): Field = Field(this.value.toString(), ArrowTypes.DoubleType)
+value class DoubleLiteral(private val value: Double) : LogicalExpression {
 
-    override fun toString(): String = this.value.toString()
+  override fun toField(context: LogicalOperator): Field {
+    return Field(this.value.toString(), Types.Double)
+  }
+
+  override fun toString(): String = this.value.toString()
 }
 
-fun lit(value: Double): DoubleLiteral = DoubleLiteral(value)
+fun lit(value: Double) = DoubleLiteral(value)
 
 @JvmInline
-value class StringLiteral(
-    val value: String,
-) : LogicalExpression {
-    override fun toField(context: LogicalOperator): Field = Field(this.value, ArrowTypes.StringType)
+value class StringLiteral(private val value: String) : LogicalExpression {
 
-    override fun toString(): String = "\"${this.value}\""
+  override fun toField(context: LogicalOperator): Field {
+    return Field(this.value, Types.String)
+  }
+
+  override fun toString(): String = "\"${this.value}\""
 }
 
-fun lit(value: String): StringLiteral = StringLiteral(value)
+fun lit(value: String) = StringLiteral(value)
 
 abstract class BinaryBooleanExpression(
-    val name: String,
-    val symbol: String,
-    val lhs: LogicalExpression,
-    val rhs: LogicalExpression,
+    private val name: String,
+    private val symbol: String,
+    private val lhs: LogicalExpression,
+    private val rhs: LogicalExpression,
 ) : LogicalExpression {
-    override fun toField(context: LogicalOperator): Field = Field(this.name, ArrowTypes.BooleanType)
 
-    override fun toString(): String = "${this.lhs} ${this.symbol} ${this.rhs}"
+  override fun toField(context: LogicalOperator): Field {
+    return Field(this.name, Types.Boolean)
+  }
+
+  override fun toString(): String = "${this.lhs} ${this.symbol} ${this.rhs}"
 }
 
 class Equals(
@@ -168,66 +182,71 @@ class Equals(
     rhs: LogicalExpression,
 ) : BinaryBooleanExpression("eq", "=", lhs, rhs)
 
-infix fun LogicalExpression.eq(rhs: LogicalExpression): Equals = Equals(this, rhs)
+infix fun LogicalExpression.eq(rhs: LogicalExpression) = Equals(this, rhs)
 
 class NotEquals(
     lhs: LogicalExpression,
     rhs: LogicalExpression,
 ) : BinaryBooleanExpression("neq", "!=", lhs, rhs)
 
-infix fun LogicalExpression.neq(rhs: LogicalExpression): NotEquals = NotEquals(this, rhs)
+infix fun LogicalExpression.neq(rhs: LogicalExpression) = NotEquals(this, rhs)
 
 class GreaterThan(
     lhs: LogicalExpression,
     rhs: LogicalExpression,
 ) : BinaryBooleanExpression("gt", ">", lhs, rhs)
 
-infix fun LogicalExpression.gt(rhs: LogicalExpression): GreaterThan = GreaterThan(this, rhs)
+infix fun LogicalExpression.gt(rhs: LogicalExpression) = GreaterThan(this, rhs)
 
 class GreaterThanOrEquals(
     lhs: LogicalExpression,
     rhs: LogicalExpression,
 ) : BinaryBooleanExpression("gte", ">=", lhs, rhs)
 
-infix fun LogicalExpression.gte(rhs: LogicalExpression): GreaterThanOrEquals = GreaterThanOrEquals(this, rhs)
+infix fun LogicalExpression.gte(rhs: LogicalExpression) = GreaterThanOrEquals(this, rhs)
 
 class LessThan(
     lhs: LogicalExpression,
     rhs: LogicalExpression,
 ) : BinaryBooleanExpression("lt", "<", lhs, rhs)
 
-infix fun LogicalExpression.lt(rhs: LogicalExpression): LessThan = LessThan(this, rhs)
+infix fun LogicalExpression.lt(rhs: LogicalExpression) = LessThan(this, rhs)
 
 class LessThanOrEquals(
     lhs: LogicalExpression,
     rhs: LogicalExpression,
 ) : BinaryBooleanExpression("lte", "<=", lhs, rhs)
 
-infix fun LogicalExpression.lte(rhs: LogicalExpression): LessThanOrEquals = LessThanOrEquals(this, rhs)
+infix fun LogicalExpression.lte(rhs: LogicalExpression) = LessThanOrEquals(this, rhs)
 
 class And(
     lhs: LogicalExpression,
     rhs: LogicalExpression,
 ) : BinaryBooleanExpression("and", "AND", lhs, rhs)
 
-infix fun LogicalExpression.and(rhs: LogicalExpression): And = And(this, rhs)
+infix fun LogicalExpression.and(rhs: LogicalExpression) = And(this, rhs)
 
 class Or(
     lhs: LogicalExpression,
     rhs: LogicalExpression,
 ) : BinaryBooleanExpression("or", "OR", lhs, rhs)
 
-infix fun LogicalExpression.or(rhs: LogicalExpression): Or = Or(this, rhs)
+infix fun LogicalExpression.or(rhs: LogicalExpression) = Or(this, rhs)
 
 abstract class BinaryArithmeticExpression(
-    val name: String,
-    val symbol: String,
-    val lhs: LogicalExpression,
-    val rhs: LogicalExpression,
+    private val name: String,
+    private val symbol: String,
+    private val lhs: LogicalExpression,
+    private val rhs: LogicalExpression,
 ) : LogicalExpression {
-    override fun toField(context: LogicalOperator): Field = Field(this.name, this.lhs.toField(context).type)
 
-    override fun toString(): String = "${this.lhs} ${this.symbol} ${this.rhs}"
+  override fun toField(context: LogicalOperator): Field {
+    val leftField = this.lhs.toField(context)
+
+    return Field(this.name, leftField.type)
+  }
+
+  override fun toString(): String = "${this.lhs} ${this.symbol} ${this.rhs}"
 }
 
 class Add(
@@ -235,65 +254,63 @@ class Add(
     rhs: LogicalExpression,
 ) : BinaryArithmeticExpression("add", "+", lhs, rhs)
 
-infix operator fun LogicalExpression.plus(rhs: LogicalExpression): Add = Add(this, rhs)
+infix operator fun LogicalExpression.plus(rhs: LogicalExpression) = Add(this, rhs)
 
 class Subtract(
     lhs: LogicalExpression,
     rhs: LogicalExpression,
 ) : BinaryArithmeticExpression("sub", "-", lhs, rhs)
 
-infix operator fun LogicalExpression.minus(rhs: LogicalExpression): Subtract = Subtract(this, rhs)
+infix operator fun LogicalExpression.minus(rhs: LogicalExpression) = Subtract(this, rhs)
 
 class Multiply(
     lhs: LogicalExpression,
     rhs: LogicalExpression,
 ) : BinaryArithmeticExpression("mul", "*", lhs, rhs)
 
-infix operator fun LogicalExpression.times(rhs: LogicalExpression): Multiply = Multiply(this, rhs)
+infix operator fun LogicalExpression.times(rhs: LogicalExpression) = Multiply(this, rhs)
 
 class Divide(
     lhs: LogicalExpression,
     rhs: LogicalExpression,
 ) : BinaryArithmeticExpression("div", "/", lhs, rhs)
 
-infix operator fun LogicalExpression.div(rhs: LogicalExpression): Divide = Divide(this, rhs)
+infix operator fun LogicalExpression.div(rhs: LogicalExpression) = Divide(this, rhs)
 
 class Modulus(
     lhs: LogicalExpression,
     rhs: LogicalExpression,
 ) : BinaryArithmeticExpression("mod", "%", lhs, rhs)
 
-infix operator fun LogicalExpression.rem(rhs: LogicalExpression): Modulus = Modulus(this, rhs)
+infix operator fun LogicalExpression.rem(rhs: LogicalExpression) = Modulus(this, rhs)
 
 abstract class AggregateExpression(
-    val name: String,
-    val input: LogicalExpression,
+    private val name: String,
+    private val input: LogicalExpression,
 ) : LogicalExpression {
-    override fun toField(context: LogicalOperator): Field = Field(this.name, this.input.toField(context).type)
 
-    override fun toString(): String = "${this.name}(${this.input})"
+  override fun toField(context: LogicalOperator): Field {
+    val leftField = this.input.toField(context)
+
+    return Field(this.name, leftField.type)
+  }
+
+  override fun toString(): String = "${this.name}(${this.input})"
 }
 
-class Sum(
-    input: LogicalExpression,
-) : AggregateExpression("SUM", input)
+class Sum(input: LogicalExpression) : AggregateExpression("SUM", input)
 
-class Min(
-    input: LogicalExpression,
-) : AggregateExpression("MIN", input)
+class Min(input: LogicalExpression) : AggregateExpression("MIN", input)
 
-class Max(
-    input: LogicalExpression,
-) : AggregateExpression("MAX", input)
+class Max(input: LogicalExpression) : AggregateExpression("MAX", input)
 
-class Avg(
-    input: LogicalExpression,
-) : AggregateExpression("AVG", input)
+class Avg(input: LogicalExpression) : AggregateExpression("AVG", input)
 
-data class Count(
-    val input: LogicalExpression,
-) : LogicalExpression {
-    override fun toField(context: LogicalOperator): Field = Field("COUNT", ArrowTypes.Int32Type)
+class Count(private val input: LogicalExpression) : LogicalExpression {
 
-    override fun toString(): String = "COUNT(${this.input})"
+  override fun toField(context: LogicalOperator): Field {
+    return Field("COUNT", Types.Int32)
+  }
+
+  override fun toString(): String = "COUNT(${this.input})"
 }
