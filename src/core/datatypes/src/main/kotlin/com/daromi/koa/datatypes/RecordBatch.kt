75a38fde68c5eecb4eecd6f2aa29dd1ef11ea7f6
daromi94 <davromero07@gmail.com>
2024-10-18 22:35:59 -0500

    style: :art: format record-batch

--- src/core/datatypes/src/main/kotlin/com/daromi/koa/datatypes/RecordBatch.kt
+++ src/core/datatypes/src/main/kotlin/com/daromi/koa/datatypes/RecordBatch.kt
@@ -1,23 +1,22 @@
 package com.daromi.koa.datatypes
 
-import org.apache.arrow.vector.types.pojo.ArrowType
-
-data class RecordBatch(
-    val schema: Schema,
-    val size: Int,
-    val columns: List<ColumnVector>,
+class RecordBatch
+private constructor(
+    private val schema: Schema,
+    private val size: UInt,
+    private val columns: List<ColumnVector>,
 ) {
-    operator fun get(index: Int): ColumnVector {
-        if (index !in 0..<this.columns.size) {
-            throw IndexOutOfBoundsException("column index $index out of bounds")
-        }
-
-        return this.columns[index]
+  operator fun get(index: Int): ColumnVector {
+    if (index !in 0..<this.columns.size) {
+      throw IndexOutOfBoundsException("column index $index out of bounds")
     }
+
+    return this.columns[index]
+  }
 }
 
 interface ColumnVector {
-    val type: ArrowType
+  val type: FieldType
 
-    operator fun get(index: Int): Any
+  operator fun get(index: Int): Any
 }
