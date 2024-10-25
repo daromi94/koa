a0561aa145852a2c74417831282c984cd4428d9a
daromi94 <davromero07@gmail.com>
2024-10-24 19:17:40 -0500

    style: :art: format build

--- src/core/settings.gradle.kts
+++ src/core/settings.gradle.kts
@@ -1,10 +1,13 @@
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
-}
+plugins { id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0" }
 
 rootProject.name = "core"
+
 include("datatypes")
+
 include("datasource")
+
 include("dataframe")
+
 include("logical-plan")
+
 include("physical-plan")
